62840b524673d3e57e7959eb6afa7409ac95322e
Santiago Sánchez <santy.8960@gmail.com>
2023-10-16 11:29:45 -0500

    locators

--- features/pageobjects/login.page.ts
+++ features/pageobjects/login.page.ts
@@ -3,26 +3,40 @@ import Page from './page';
 
 class LoginPage extends Page {
 
-    public get inputUsername () {
-        return $('#username');
+    public get startHere() {
+        return $('(// * [contains (text (), "Start here")])[2]');
     }
 
-    public get inputPassword () {
-        return $('#password');
+    public get inputYourName() {
+        return $('#ap_customer_name');
     }
 
-    public get btnSubmit () {
-        return $('button[type="submit"]');
+    public get inputEmail() {
+        return $('#ap_email');
     }
 
-    public async login (username: string, password: string) {
-        await this.inputUsername.setValue(username);
-        await this.inputPassword.setValue(password);
-        await this.btnSubmit.click();
+    public get inputPassword() {
+        return $('#ap_password');
+    }
+
+    public get inputPasswordCheck() {
+        return $('#ap_password_check');
+    }
+    
+    public get continueButton() {
+        return $('//input[@id="continue"]')
+    }
+
+    public get inputEnterOTP() {
+        return $('//input[@name="code"]') // //div[@id="cvf-input-code-container"]
+    }   
+
+    public get createAccountButton() {
+        return $('//input [contains(@aria-labelledby, "cvf-submit-otp-button-announce")]')
     }
 
     public open () {
-        return super.open('login');
+        return super.open();
     }
 }
 
--- features/pageobjects/page.ts
+++ features/pageobjects/page.ts
@@ -2,7 +2,7 @@ import { browser } from '@wdio/globals'
 
 export default class Page {
 
-    public open (path: string) {
-        return browser.url(`https://the-internet.herokuapp.com/${path}`)
+    public open () {
+        browser.url(`https://www.amazon.com/`)
     }
 }

5b41873ba68a8fc2db6c1abaf9688a8cd6976911
Santiago Sánchez <santy.8960@gmail.com>
2023-10-25 18:43:47 -0500

    update

--- features/Tasks/productVerify.tasks.ts
+++ features/Tasks/productVerify.tasks.ts
@@ -1,20 +1,22 @@
 import { assert } from "chai";
-import { ProductVerifyPage } from "../pageobjects/productVerify.page";
+import productVerifyPage, { ProductVerifyPage } from "../pageobjects/productVerify.page";
 import { browser } from '@wdio/globals'
+import { expect } from 'chai';
 
 
 export class ProductVerifyTask extends ProductVerifyPage {
 
-       async checkParagraph():Promise<boolean>  {
+    async checkParagraph():Promise<boolean>  {
 
         await browser.pause(10000)
         const parrafos = await this.featuresMenu;
-
-        parrafos.forEach(async (element: any) => {
-            console.log(await element.getText())
-        });
-
         assert.equal(parrafos.length, 5);
+        return true;
+    }
+
+    async ramCheck():Promise<boolean> {
+        const ram = productVerifyPage.ram.getText();
+        await expect(ram).to.equal("12.00");
         return true;
     }
 }
